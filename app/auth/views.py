ac1d2dd80ee0ca1a7de48f43a981e7dcaceaf3eb
joseck ogachi <jogachi4@gmail.com>
2019-02-12 17:15:25 +0300

    add changes to views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,5 +3,14 @@ from . import auth
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
+         log_in = LogIn()
+     title = 'Login'
+     if log_in.validate_on_submit():
+          user = User.query.filter_by(username = log_in.username.data).first()
+          if user is not None and bcrypt.check_password_hash(user.password, log_in.password.data):
+               login_user(user,log_in.remember_me.data)
+               return redirect(request.args.get('next') or url_for('main.index'))
+
+          flash('The username or password you entered is incorrect.')
 
      return render_template('auth/login.html', login = log_in, title = title)
