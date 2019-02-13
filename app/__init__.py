2861e384a1a66671187df001b86da70c9d505e2a
joseck ogachi <jogachi4@gmail.com>
2019-02-13 05:23:58 +0300

    additions to files

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,6 @@ from flask_login import LoginManager
 
 
 db = SQLAlchemy()
-# bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.enter'
@@ -27,7 +26,7 @@ def create_app(config_name):
    # Registering the blueprint
    from .main import main as main_blueprint
    app.register_blueprint(main_blueprint)
-   # from .auth import auth as auth_blueprint
-   # app.register_blueprint(auth_blueprint,url_prefix = '')
+   from .auth import auth as auth_blueprint
+   app.register_blueprint(auth_blueprint,url_prefix = '')
 
    return app
